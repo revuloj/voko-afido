b9b0173c58fe6fbca9a8ebd2bb7845b11eb370c5
Wolfram Diestel <diestel@steloj.de>
2019-05-19 11:58:06 +0200

    kreu pliajn ujojn mailatt, xml, prcmail ĉe ekiro de afido

--- bin/setup_var.sh
+++ bin/setup_var.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
+set -x
+
 # FARENDA: legu var el config:
 # var=$(cat /voko-afido.var_afido)
 var=/var/afido
 
-mkdir -p ${var}/tmp
-mkdir -p ${var}/log
+mkdir -p ${var}/tmp/mailatt
+mkdir -p ${var}/tmp/xml
+
+#mkdir -p ${var}/log
 mkdir -p ${var}/log/oldmail
 mkdir -p ${var}/log/errmail
+mkdir -p ${var}/log/prcmail
+
 chown -R afido.users ${var}
+
