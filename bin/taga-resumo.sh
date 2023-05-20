e6d0356a70f47490899382233253793483bde097
Wolfram Diestel <diestel@steloj.de>
2023-05-20 16:48:58 +0200

    preparis skripton por ttga resumo

---
+++ bin/taga-resumo.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# kreas tagan resumon de prokoloj de la redaktoservo (retpoŝta)
+# la ideo estas post elfiltro de interesaj linioj ankaŭ sendi ĝin al administranto per
+# per ŝaltilo -a ni rigardas la protokolojn de la antaŭa tago
+#
+# $ taga-resumo.sh  mail -s "redakto-servo - taga resumo" mia@poshtservo.org
+
+log_prefix="${HOME}/log/redsrv-"
+
+if [[ "$1" == "-a" ]]; then
+    date=$(date +"%Y%m%d" -d "1 day ago")
+else
+    date=$(date +"%Y%m%d")
+fi
+
+grep "(fatal|fail)" ${log_prefix}${date}*
