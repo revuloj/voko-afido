35e018a9902bda4591396b403d79ca24326e74fc
Wolfram Diestel <diestel@steloj.de>
2025-05-02 10:32:03 +0200

    ni bezonas entrypoint por la testo

--- tst/docker-test.sh
+++ tst/docker-test.sh
@@ -8,7 +8,7 @@ set -e
 docker_image="${1:-voko-afido:latest}"
 
 # lanĉi la test-procezujon
-docker run --entrypoint '' ${docker_image} bash -c "ls -l && ls -l dict &&\
+docker run ${docker_image} bash -c "ls -l && ls -l dict &&\
    [ -f dict/dtd/vokoxml.dtd ] || exit 1 &&\
    perl -MMIME::Entity -MAuthen::SASL::Perl -MIO::Socket::SSL -e1 &&\
    perl -c /usr/local/bin/processgist.pl && perl -c /usr/local/bin/processsubm.pl &&\
